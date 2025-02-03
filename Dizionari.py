27d572b6d44f99c3439b0e5ac876085b9d91e640
Paolod01100110 <paolod04@icloud.com>
2025-02-03 17:26:04 +0100

    Add files via upload

---
+++ Dizionari.py
@@ -0,0 +1,15 @@
+studente = {
+ "nome": "Alice",
+ "età": 20,
+ "sesso": "Femmina"
+ } 
+
+print(studente["nome"])   
+
+print(studente["età"]) 
+
+studente["età"] = 21
+print(studente)  
+
+studente["città"] = "Roma"
+print(studente)  
