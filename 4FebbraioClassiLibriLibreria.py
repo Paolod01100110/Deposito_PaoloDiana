a6253f28b53ef05406df8ee23e716c47ab147326
Paolod01100110 <paolod04@icloud.com>
2025-02-04 15:49:10 +0100

    Add files via upload

--- 4FebbraioClassiLibriLibreria.py
+++ 4FebbraioClassiLibriLibreria.py
@@ -34,3 +34,30 @@ class Libreria:
             print("Catalogo della libreria:")
             for libro in self.catalogo:
                 print(libro.descrizione())
+                
+# Creazione di una libreria
+libreria = Libreria()
+
+# Creazione e aggiunta di libri
+libro1 = Libro("Il Signore degli Anelli", "J.R.R. Tolkien", "9788804668239")
+libro2 = Libro("1984", "George Orwell", "9780451524935")
+libro3 = Libro("1984", "George Orwell", "9780141036144")
+
+libreria.aggiungi_libro(libro1)
+libreria.aggiungi_libro(libro2)
+libreria.aggiungi_libro(libro3)
+
+# Mostrare il catalogo
+libreria.mostra_catalogo()
+
+# Ricerca per titolo
+risultati = libreria.cerca_per_titolo("1984")
+print("\nRisultati ricerca per titolo '1984':")
+for libro in risultati:
+    print(libro.descrizione())
+
+# Rimozione di un libro
+libreria.rimuovi_libro("9780451524935")
+
+# Mostrare il catalogo aggiornato
+libreria.mostra_catalogo()
